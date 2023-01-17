1b6944a1d443919d7a59848506f4caa109c13248
Rajsk <rakibulsk.cse@gmail.com>
2023-01-17 15:34:10 +0530

    solution

--- App.js
+++ App.js
@@ -1,1 +1,6 @@
-
+const button=document.querySelector("button");
+const changeBackground=()=>{
+  const hexval = Math.floor(Math.random() * 0xffffff).toString(16);
+  document.body.style.background = `#${hexval}`;
+}
+button.addEventListener("click", changeBackground);
